00f3c2e658540407d66c1831cd5eea3fe3302cd4
sw <swang.arch@hotmail.com>
2024-10-31 23:41:33 +0100

    make functions

--- philo/main.c
+++ philo/main.c
@@ -96,33 +96,6 @@ int	*init_tab(int num_philo, int value)
 // 	return (number_eaten);
 // }
 
-void init_vars_setup(t_args *args, char **av, int i)
-{
-	args->philo_index = i;
-	args->number_of_philosophers = ft_atoi(av[1]);
-	args->time_to_die = ft_atoi(av[2]) * 1000;
-	args->time_to_eat = ft_atoi(av[3]) * 1000;
-	args->time_to_sleep = ft_atoi(av[4]) * 1000;
-}
-
-void init_vars_shared(t_args *args, pthread_mutex_t *mutex_printf, pthread_mutex_t **mutex_forks, int *alive, int *fork_ontable)
-{
-	args->mutex_printf = mutex_printf;
-	args->number_eaten = 0;
-	args->mutex_fork = mutex_forks;
-	if (!args->mutex_fork)
-		return ; /////////////protect
-	args->fork_ontable = fork_ontable; /////////////////////protect
-	args->alive = alive;
-}
-
-void init_sim_state(t_args *args, int *sim_end, time_t start_time, int num_must_eat, int *number_eaten)
-{
-	args->sim_end = sim_end;
-	args->start_time = start_time;
-	args->number_of_times_each_philosopher_must_eat = num_must_eat;
-	args->number_eaten = number_eaten;
-}
 
 void init_vars_monitor(t_args_monitor *args_monitor, pthread_mutex_t *mutex_printf, int *alive, int *sim_end,int num_must_eat, int *number_eaten, int num)
 {
@@ -169,7 +142,7 @@ typedef struct s_state{
 	int	*fork_ontable;
 	int	*alive;                  //0 die 1 alive 2 sim_end
 	int *number_eaten;
-	int sim_end;
+	int *sim_end;
 	time_t start_time;
 } t_state;
 
@@ -182,7 +155,10 @@ void state_setup(t_setup *set, t_state *state)
 	state->fork_ontable = init_tab(num, 1);  //protect
 	state->alive = init_tab(num, 1);
 	state->number_eaten = init_tab(num, 0);
-	state->sim_end = 0;   ///////////////////////////can be removed
+	state->sim_end = malloc(sizeof(int));
+	if(!state->sim_end)
+		return ;   ///////////////////////////can be removed
+	*(state->sim_end) = 0;
 	state->start_time = now_time();
 }
 
@@ -200,9 +176,85 @@ void mutex_setup(t_setup *set, t_mutex *mutexes)
 	if (!mutexes->mutex_printf)
 		return ;////////////protect
 	pthread_mutex_init(mutexes->mutex_printf, NULL);
-	mutexes->mutex_forks = init_mutex_forks(num);
+	mutexes->mutex_forks = init_mutex_forks(num);//protect
 }
 
+
+void init_vars_setup(t_args *args, char **av, int i)
+{
+	args->philo_index = i;
+	args->number_of_philosophers = ft_atoi(av[1]);
+	args->time_to_die = ft_atoi(av[2]) * 1000;
+	args->time_to_eat = ft_atoi(av[3]) * 1000;
+	args->time_to_sleep = ft_atoi(av[4]) * 1000;
+}
+
+void init_vars_shared(t_args *args, pthread_mutex_t *mutex_printf, pthread_mutex_t **mutex_forks, int *alive, int *fork_ontable)
+{
+	args->mutex_printf = mutex_printf;
+	args->number_eaten = 0;
+	args->mutex_fork = mutex_forks;
+	if (!args->mutex_fork)
+		return ; /////////////protect
+	args->fork_ontable = fork_ontable; /////////////////////protect
+	args->alive = alive;
+}
+
+void init_sim_state(t_args *args, int *sim_end, time_t start_time, int num_must_eat, int *number_eaten)
+{
+	args->sim_end = sim_end;
+	args->start_time = start_time;
+	args->number_of_times_each_philosopher_must_eat = num_must_eat;
+	args->number_eaten = number_eaten;
+}
+
+void init_args_philo(t_args *args, t_setup *set, t_state *state, t_mutex *mutex, int index)
+{
+	if (!args || !set || !state || !mutex)
+		return ;
+	args->philo_index = index;
+	args->number_of_philosophers = set->number_of_philosophers;
+	args->time_to_die = set->time_to_die;
+	args->time_to_eat = set->time_to_eat;
+	args->time_to_sleep = set->time_to_sleep;
+	args->number_of_times_each_philosopher_must_eat = set->number_of_times_each_philosopher_must_eat;
+
+	args->fork_ontable = state->fork_ontable;
+	args->alive = state->alive;
+	args->number_eaten = state->number_eaten;
+	args->sim_end = state->sim_end;
+	args->start_time = state->start_time;
+
+	args->mutex_fork = mutex->mutex_forks;
+	args->mutex_printf = mutex->mutex_printf;
+}
+
+void **set_args_philo(t_setup *set, t_state *state, t_mutex *mutexes)
+{
+	int i;
+	void	**arg_tab;
+	int num;
+
+	num = set->number_of_philosophers;
+
+	arg_tab = (void **)malloc(sizeof(t_args*) * (num + 1));
+	if (arg_tab == NULL)
+		return (NULL);
+	i = 0;
+	while (i < num)
+	{
+		arg_tab[i] = (t_args *)malloc(sizeof(t_args));
+		if (arg_tab[i] == NULL)
+		{
+			free_tab(arg_tab);
+			return (NULL);
+		}
+		init_args_philo(arg_tab[i], set, state, mutexes, i);
+		i++;
+	}
+	arg_tab[i] = NULL;
+	return (arg_tab);
+}
 
 
 int run_simulation(int ac, char **av)
@@ -217,49 +269,47 @@ int run_simulation(int ac, char **av)
 
 
 
-
 	int num_must_eat;
 	num_must_eat = set.number_of_times_each_philosopher_must_eat;
-
-	time_t start_time;
-
-	start_time = now_time();
 	int	num_philo = ft_atoi(av[1]);
 
 	pthread_mutex_t mutex_printf;
-	pthread_mutex_t **mutex_forks;
+	// pthread_mutex_t **mutex_forks;
 	pthread_mutex_init(&mutex_printf, NULL);
-	mutex_forks = init_mutex_forks(num_philo);
+	// mutex_forks = init_mutex_forks(num_philo);
 
-	int i = 0;
 	int	sim_end = 0;
-	int	*fork_ontable = state.fork_ontable;
 	int	*alive = state.alive;
 	int *number_eaten = state.number_eaten;
-	/*----------------Set_vars_for_each_philo--------------------------*/
-	void	**arg_tab; ///MAKE ARG_TAB 
-	arg_tab = (void **)malloc(sizeof(t_args) * (num_philo + 1));
-	if (arg_tab == NULL)
-		return (1);
 
-	i = 0;
-	while (i < num_philo)
-	{
-		arg_tab[i] = (t_args *)malloc(sizeof(t_args));
-		if (arg_tab[i] == NULL)
-		{
-			free_tab(arg_tab);
-			return (1);
-		}
-		init_vars_setup((t_args *)arg_tab[i], av, i);
-		init_vars_shared((t_args *)arg_tab[i], &mutex_printf, mutex_forks, alive, fork_ontable);
-		init_sim_state((t_args *)arg_tab[i], &sim_end, start_time, num_must_eat, number_eaten);
-		i++;
-	}
-	arg_tab[i] = NULL;
+
+	/*----------------Set_vars_for_each_philo--------------------------*/
+
+	void	**arg_tab = set_args_philo(&set, &state, &mutexes); 
+
+	// int i;
+	// void	**arg_tab; 
+
+	// arg_tab = (void **)malloc(sizeof(t_args) * (num_philo + 1));
+	// if (arg_tab == NULL)
+	// 	return (1);
+	// i = 0;
+	// while (i < num_philo)
+	// {
+	// 	arg_tab[i] = (t_args *)malloc(sizeof(t_args));
+	// 	if (arg_tab[i] == NULL)
+	// 	{
+	// 		free_tab(arg_tab);
+	// 		return (1);
+	// 	}
+	// 	init_args_philo(arg_tab[i], &set, &state, &mutexes, i);
+	// 	i++;
+	// }
+	// arg_tab[i] = NULL;
 	/*----------------Set_vars_for_each_philo--------------------------*/
 	/*----------------Create_threads--------------------------*/
 	
+	int i;
 	pthread_t	monitor;
 	t_args_monitor monitor_vars;
 
@@ -290,10 +340,10 @@ int run_simulation(int ac, char **av)
 	}
 	
 	/*----------------Join_threads--------------------------*/
-	destroy_mutex_forks(num_philo, mutex_forks);
+	//destroy_mutex_forks(num_philo, mutex_forks);
 	/////pthread_mutex_destroy(&mutex_forks[k]);
 	free_tab(arg_tab);//need to free tab
-	free(fork_ontable);
+	//free(fork_ontable);
 	return (0);
 }
 
